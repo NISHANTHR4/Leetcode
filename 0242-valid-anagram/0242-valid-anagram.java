e959742245ec95ee6a63c480d66188630b6184dd
NISHANTHR4 <97089406+NISHANTHR4@users.noreply.github.com>
2024-12-05 14:02:23 +0530

    Time: 5 ms (54.11%), Space: 44.9 MB (15.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())
+        if(s==null || t==null || s.length()!=t.length())
         {
             return false;
         }
@@ -8,7 +8,7 @@ class Solution {
         char[] ch2=t.toCharArray();
         Arrays.sort(ch1);
         Arrays.sort(ch2);
-        for(int i=0;i<ch1.length;i++)
+        for(int i=0;i<s.length();i++)
         {
             if(ch1[i]!=ch2[i])
             {
@@ -16,6 +16,6 @@ class Solution {
             }
         }
         return true;
-
+        
     }
 }
